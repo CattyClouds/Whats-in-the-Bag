e1413111f614972bf1f77147c337a95c1c9d212c
CattyClouds <cattyclouds@gmail.com>
2016-08-30 07:56:01 -0500

    Adding empty project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace What_s_in_the_Bag
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+		}
+	}
+}
