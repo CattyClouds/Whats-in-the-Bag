7ad8103fa7ddefb8f27c5f6782af80ce8a28e0cb
CattyClouds <cattyclouds@gmail.com>
2016-09-11 08:25:31 -0500

    Added testing functionality and output

--- Program.cs
+++ Program.cs
@@ -8,9 +8,71 @@ namespace What_s_in_the_Bag
 {
 	class Program
 	{
+		public static int[] TileAmounts { get; set; }
+
 		static void Main(string[] args)
 		{
+			string dash = String.Concat(Enumerable.Repeat("-", 32));
+			Console.WriteLine($"What's in the bag?{Environment.NewLine}{dash}");
 
+			Console.Write("Tiles out of the bag: ");
+			Input("AEERTYOXMCNB_S"); // Sample input
+									 //Input(Console.ReadLine());
+
+			// Debug outputs
+			string theCount = string.Join(",", TileAmounts.ToArray());
+			Console.WriteLine($"Total amount of each tile in bag: {theCount}");
+			Console.WriteLine($"Total tiles in bag: {TileCount()}");
+
+			// End of program
+			Console.Write($"{dash}{Environment.NewLine}Press any key to exit...");
+			Console.ReadKey();
+		}
+
+		static List<char> Input(string tilesOutOfBag)
+		{
+			List<char> bag = new List<char>(); // Bag is empty
+
+			string tiles = "ABCDEFGHIJKLMNOPQRSTUVWXYZ_";
+
+			int[] tileAmounts = {
+				9,2,2,  // A,B,C,
+				4,12,2, // D,E,F,
+				3,2,9,  // G,H,I,
+				1,1,4,  // J,K,L,
+				2,6,8,  // M,N,O,
+				2,1,6,  // P,Q,R,
+				4,6,4,  // S,T,U,
+				2,2,1,  // V,W,X,
+				2,1,2   // Y,Z,_
+				};
+
+			TileAmounts = tileAmounts; // Set property
+
+			// Debug: Adding all tiles to bag once
+			for (int i = 0; i < tiles.Length; i++)
+			{
+				bag.Add(tiles[i]);
+			}
+
+			//foreach (var tile in tiles)
+			//{
+			//	bag.Add(tile);
+			//}
+
+			string bagItems = string.Join(",", bag.ToArray());
+			Console.WriteLine(bagItems);
+			return bag;
+		}
+
+		static int TileCount()
+		{
+			int count = 0;
+			for (int i = 0; i < TileAmounts.Length; i++)
+			{
+				count += TileAmounts[i];
+			}
+			return count; // Should be 100 by default
 		}
 	}
 }
